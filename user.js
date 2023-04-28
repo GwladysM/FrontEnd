974fb304807f67f3707a7041ce300bc11c909913
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-04-27 20:12:12 -0400

    commit 4 avancée sur page en mode edit et log out

--- user.js
+++ user.js
@@ -1,34 +1,35 @@
-// -> importer les infos de l'api (+ userId et Token)
-// -> SI identifiant ok alors la page s'ouvre
-// SINON
-// -> SI identifiant pas OK alors mettre un msg d'erreur
-// -> connexion ok, alors rediriger vers page index.html
-function authentification() {
-    fetch("http://localhost:5678/api/users/login", {
+// Création de la fonction d'autentification :
+async function authentification() {
+    const user = {
+        email: email.value,
+        password: password.value
+    }
+ 
+    const response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
             "Content-type": "application/json",
             Accept: "application/json"
         },
-        body: JSON.stringify({
-            email: email.value,
-            password: password.value
-        })
+        body: JSON.stringify(user)
     })
-        .then((response) => response.json())
-//        .then((data) => {
-//            localStorage.setItem("token", data.token);
-//            window.location.href = "index.html";
-//        })
+    const data = await response.json();
+    const userToken = data.token;
+    // SI identifiant et mot de passe OK,
+    // ALORS enregitrer token + redirection page index.html
+    // SINON afficher msg erreur
+    if (userToken) {
+        localStorage.setItem("token", userToken);
+        window.location.href = "index.html";
+    } else {
+        alert("Erreur dans l’identifiant ou le mot de passe");
+    }
 };
 
-const seConnecter = document.getElementById("loginform");
-seConnecter.addEventListener("submit", (event) => {
+
+// Création de l'évènement authentification et retour page d'accueil en mode admin :
+const logIn = document.getElementById("loginform");
+logIn.addEventListener("submit", (event) => {
     event.preventDefault();
     authentification();
-//    if (token == true) {
-//        console.log("connexion ok");
-//    } else {
-//        console.log("identifiant ou mot de passe incorrect");
-//    }
 });
--- works.js
+++ works.js
@@ -3,13 +3,15 @@ fetch('http://localhost:5678/api/works')
 
 const reponse = await fetch('http://localhost:5678/api/works');
 
+//
 //création de la constante works qui représente les projets de l'architecte :
 const works = await reponse.json();
 
+//
 // Génération des éléments qui composent chaque projet de l'architecte :
 
-function genererWorks(works){
-    for(let i = 0; i < works.length; i++) {
+function generateWorks(works) {
+    for (let i = 0; i < works.length; i++) {
 
         const project = works[i];
 
@@ -31,47 +33,75 @@ function genererWorks(works){
         // Rattacher les balises au projet :
         workElement.appendChild(imageUrlElement);
         workElement.appendChild(titleElement);
-   }
+    }
 
 }
 
-genererWorks(works);
+generateWorks(works);
 
+//
 // Création des fonctions FILTER pour chaque boutton :
 
 const boutonTous = document.querySelector(".btn-tous");
-boutonTous.addEventListener("click", function() {
-    const worksTous = works.filter(function(work) {
+boutonTous.addEventListener("click", function () {
+    const worksTous = works.filter(function (work) {
         return work.categoryId;
     });
     // Effacer la page pour la régénérer et afficher le bon filtre :
     document.querySelector(".gallery").innerHTML = "";
-    genererWorks(worksTous);
+    generateWorks(worksTous);
 });
 
 const boutonObjets = document.querySelector(".btn-objets");
-boutonObjets.addEventListener("click", function() {
-    const worksObjets = works.filter(function(work) {
+boutonObjets.addEventListener("click", function () {
+    const worksObjets = works.filter(function (work) {
         return work.categoryId == 1;
     });
     document.querySelector(".gallery").innerHTML = "";
-    genererWorks(worksObjets);
+    generateWorks(worksObjets);
 });
 
 const boutonAppartements = document.querySelector(".btn-appartements");
-boutonAppartements.addEventListener("click", function() {
-    const worksAppartements = works.filter(function(work) {
+boutonAppartements.addEventListener("click", function () {
+    const worksAppartements = works.filter(function (work) {
         return work.categoryId == 2;
     });
     document.querySelector(".gallery").innerHTML = "";
-    genererWorks(worksAppartements);
+    generateWorks(worksAppartements);
 });
 
 const boutonHotels = document.querySelector(".btn-hotels-restaurants");
-boutonHotels.addEventListener("click", function() {
-    const worksHotels = works.filter(function(work) {
+boutonHotels.addEventListener("click", function () {
+    const worksHotels = works.filter(function (work) {
         return work.categoryId == 3;
     });
     document.querySelector(".gallery").innerHTML = "";
-    genererWorks(worksHotels);
+    generateWorks(worksHotels);
 });
+
+//
+// Récupération du token :
+const userToken = window.localStorage.getItem("token");
+
+// Création du bouton Log-Out :
+function logOut() {
+    const logOut = document.getElementById("loginout");
+    logOut.innerText = "logout";
+
+    logOut.addEventListener("click", function () {
+        localStorage.removeItem("token");
+    });
+};
+
+if (userToken) {
+    logOut();
+}
+
+// Création de la bannière Mode Edition :
+const banner = document.querySelector("div");
+
+banner.style.backgroundColor = "rgb(0,0,0)"
+banner.style.height = "50px"
+
+const publierChgts = document.createElement('p')
+publierChgts.innerText = "publier les changements"
